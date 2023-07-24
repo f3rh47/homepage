fb43487020c0bec0027a765139b42d0ef44911b3
f3rh47 <ferhatincekara@gmail.com>
2023-07-24 14:24:52 +0300

    feat: add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
+import About from '@/components/About';
+import ImagesStories from '@/components/ImagesStories';
+import LetsTalk from '@/components/LetsTalk';
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="absolute flex min-h-full justify-between p-12 max-lg:flex-col max-lg:items-center max-lg:text-center max-sm:px-6 max-sm:pt-0">
+      <div className="absolute left-1/2 top-1/2 w-1/4 -translate-x-1/2 -translate-y-1/2 max-xl:w-[350px] max-lg:relative max-lg:left-auto max-lg:top-auto max-lg:transform-none max-sm:w-[calc(100%+theme(space.12))]">
+        <ImagesStories />
+      </div>
+
+      <About />
+      <LetsTalk />
+    </div>
+  );
 }
