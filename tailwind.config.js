c559eb0380aceb638648b2e4e76fecb805026d3f
f3rh47 <ferhatincekara@gmail.com>
2023-07-24 13:51:39 +0300

    feat: add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,14 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: '#ffffff',
+      'cod-gray': '#161616',
+      gray: '#7e7e7e',
+      'sweet-corn': '#fde68a',
     },
   },
   plugins: [],
-}
+};
